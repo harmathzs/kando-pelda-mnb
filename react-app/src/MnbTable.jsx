1d010c1d548c67e7629cb4e7ba9775b923e1f148
Zsolt Harmath <harmathzs@gmail.com>
2025-08-14 00:46:26 +0200

    Show data length

--- react-app/src/MnbTable.jsx
+++ react-app/src/MnbTable.jsx
@@ -7,14 +7,27 @@ export default class MnbTable extends React.Component {
         <Table striped bordered hover variant="dark">
             <thead>
                 <tr>
-                <th>#</th>
-                <th>Currency</th>
-                <th>Unit</th>
-                <th>Rate</th>
+                    <th>#</th>
+                    <th>Currency</th>
+                    <th>Unit</th>
+                    <th>Rate</th>
                 </tr>
             </thead>
             <tbody>
-                {this.props.data}
+                <tr>
+                    <td>
+                        {this.props.data.length}
+                    </td>
+                    <td>
+                        {this.props.data.length}
+                    </td>
+                    <td>
+                        {this.props.data.length}
+                    </td>
+                    <td>
+                        {this.props.data.length}
+                    </td>                                                            
+                </tr>        
             </tbody>
         </Table>
     }
